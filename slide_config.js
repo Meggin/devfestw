146291342831eaf9f239e882ca50edb0bf6e24a5
Meggin Kearney <meggin.kearney@gmail.com>
2013-03-14 11:04:52 -0700

    First push of devfestw presentation

---
+++ slide_config.js
@@ -0,0 +1,28 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Chrome Apps',
+    subtitle: 'Bridge between Native and the Web',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/chrome-logo-tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Meggin Kearney',
+    gplus: 'http://plus.google.com/1234567890',
+    title: 'Technical Writer',
+    company: 'Google Chrome Team',
+    github: 'https://github.com/Meggin'
+  }]
+};
+
